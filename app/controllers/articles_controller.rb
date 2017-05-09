b0c7368ee6a20fb838d062b98e205bc6b4cb5e19
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-05-09 16:51:24 +0800

    params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
   # only public methods can be actions for controllers
   def new
+
+  end
+
+  #  Rails by default returns 204 No Content response for an action if we don't specify what the response should be
+  def create
+    render plain: params[:article].inspect
   end
 end
