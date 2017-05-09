270a594ada8715a726a0a04357769ba189abe476
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-05-09 17:29:56 +0800

    solve ForbiddenAtribute issue

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
 
     # every Rails model can be initialized with its respective attributes, which are automatically mapped to the respective database columns.
-    @article = Article.new(params[:article])
+    @article = Article.new(params.require(:article).permit(:title, :text))
     @article.save
     redirect_to @article
   end
