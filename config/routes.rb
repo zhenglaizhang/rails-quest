802a4f101bdcf8b3e4694083891a52846f476fa0
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-05-10 23:30:54 +0800

    add store

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  # create store_index_path & store_index_url accessor methods
+  root 'store#index', as: 'store_index'
+
   resources :products
+
+
+  # root 'welcome#index'
   get 'welcome/index'
 
   get 'say/hello'
@@ -9,8 +16,6 @@ Rails.application.routes.draw do
   get 'say/goodbye'
 
   # todo where to put root route?? order matters??
-  # root 'welcome#index' tells Rails to map requests to the root of the application to the welcome controller's index action
-  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
