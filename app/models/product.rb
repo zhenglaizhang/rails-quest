6c7af60106a8dcef1d67532b19052176eedb2599
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-05-10 23:21:19 +0800

    .

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   # todo RE refresh
 
   validates :title, :description, :image_url, presence: true
+  # price is at least 1 cent, as db only store 2 digits after decimal point
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
